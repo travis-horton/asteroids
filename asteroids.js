61dd82d2cc4bf9d546a65b99aba11c391327f8e7
kiddspazz <travishorton.pianist@gmail.com>
2019-03-06 16:44:05 -0500

    mind blown into different files

--- asteroids.js
+++ asteroids.js
@@ -181,14 +181,6 @@
       }
     };
 
-    function rotate(point, d) {
-      let angle = d * Math.PI/180;
-      let newPoint = {x: 0, y: 0}
-      newPoint.x = (point.x * (Math.cos(angle)) - point.y * (Math.sin(angle)));
-      newPoint.y = (point.y * (Math.cos(angle)) + point.x * (Math.sin(angle)));
-      return newPoint;
-    }
-
     function Keyboarder() {
       var keyState = {};
 
---
+++ rotate.js
@@ -0,0 +1,7 @@
+function rotate(point, d) {
+  let angle = d * Math.PI/180;
+  let newPoint = {x: 0, y: 0}
+  newPoint.x = (point.x * (Math.cos(angle)) - point.y * (Math.sin(angle)));
+  newPoint.y = (point.y * (Math.cos(angle)) + point.x * (Math.sin(angle)));
+  return newPoint;
+}
