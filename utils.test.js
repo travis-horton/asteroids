c4a25f58127967ed4fae8bd7a3fda3565884f335
kiddspazz <travis@travish.com>
2022-06-26 21:41:05 -0600

    Add test for "rotate"

--- utils.test.js
+++ utils.test.js
@@ -1,4 +1,15 @@
-import { max2, getVectorLength } from './utils';
+import { max2, getVectorLength, rotate } from './utils';
+
+describe('#rotate', () => {
+  it('rotates a point around the origin theta degrees', () => {
+    const originalPoint = { x: 0, y: 1 };
+    const rotatedPoint = rotate(originalPoint, 180);
+    // Because of floating point numbers, these will be imprecise.
+    expect(rotatedPoint).toMatchObject(
+      { x: expect.closeTo(0, 5), y: expect.closeTo(-1, 5) },
+    );
+  });
+});
 
 describe('#getVectorLength', () => {
   it('calculates the length of a vector', () => {
